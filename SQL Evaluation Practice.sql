c9914b08610a53679693709a226ba09d0f3e9898
Abdullah Al Noman <165512794+nomancsediu@users.noreply.github.com>
2025-06-24 23:17:05 +0600

    Add files via upload

---
+++ SQL Evaluation Practice.sql
@@ -0,0 +1,121 @@
+#Database Level
+#--------------
+create database CollegeDB;
+Drop database CollegeDB;
+
+create database CollegeDB;
+use CollegeDB;
+show databases;
+show databases like 'CollegeDB';
+
+#Table Operations
+#----------------
+#Student Table
+create table student (
+      student_id int,
+      name varchar(255),
+      age int,
+      city varchar(255)
+);
+show tables;
+select * from student;
+
+#Course Table
+-- create table course(
+--       course_id int,
+--       course_name varchar(255),
+--       credit int
+-- );
+-- select * from course;
+
+#Enrollment table
+create table enrollment(
+enroll_id int,
+student_id int,
+course_id int,
+semester varchar(255)
+);
+select * from enrollment;
+
+#Instructor Table
+create table instructor(
+instructor_id int,
+name varchar(255),
+department varchar(255)
+);
+select * from instructor;
+
+Drop table enrollment;
+show tables;
+
+Alter table student
+add email varchar(255);
+select * from student;
+
+Alter table student
+drop age;
+select * from student;
+
+Drop table course;
+
+#Constraint apply
+create table course(
+      course_id int,
+      course_name varchar(255),
+      credit int,
+      check (credit>0)
+);
+
+alter table student
+modify column student_id int primary key;
+
+create table enrollment(
+enroll_id int,
+student_id int,
+course_id int,
+semester varchar(255),
+foreign key (student_id) references student(student_id)
+);
+
+alter table instructor
+modify name varchar(255) not null unique;
+
+alter table instructor
+modify department varchar(255) default 'CSE';
+
+create index idx on student (name);
+
+alter table enrollment
+modify enroll_id int primary key auto_increment;
+select * from student;
+
+alter table student
+drop column email;
+alter table student
+add column age int after name;
+#DML 
+insert into student(student_id,name,age,city) values
+(1,'Abdullah Al Noman',23,'Kushtia'),
+(2,'Lamia Aktar',23,'Chuadanga'),
+(3,'Supan Roy',23,'Bagerhat'),
+(4,'Samira Haque Vabna',23,'Bogura'),
+(5,'Halima Akter',23,'Dhaka');
+
+select * from student;
+
+#Update
+update student
+set city = 'Kushtia'
+where student_id = '2';
+select * from student;
+
+#Delete
+delete from student 
+where name = 'Halima Akter';
+select * from student;
+
+
+
+ 
+
+
